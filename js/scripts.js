70d1d337ced153342f0abbdae61bc6b369292c23
Lulu <lulumutuli24@gmail.com>
2018-02-13 11:19:48 +0300

    subtraction changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-var add = function(number1, number2, number3) {
-return number1 + number2 + number3;
+var subtract = function(number1, number2) {
+return number1 - number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var number3 = parseInt(prompt("Enter a third number:"));
 
-alert(add(number1, number2, number3));
+
+ var result = subtract(number1, number2);
+alert(result);
