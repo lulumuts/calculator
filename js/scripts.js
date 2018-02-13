70ff1f4abfaac404e91d3bb98718e0ac65c53929
Lulu <lulumutuli24@gmail.com>
2018-02-13 11:54:06 +0300

    fahrenheit calculation  exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 var temp = function(number1) {
-return (number1 * 9/5 + 32);
+return (number1 - 32) * 5/9 ;
 };
 
-var number1 = parseInt(prompt("Enter temperature in C"));
+
+var number1 = parseInt(prompt("Enter temperature in F"));
 
 
  var result = temp(number1);
