9ac57075cd9f15cf54f1710d39b0d2d5e53f5dab
Lulu <lulumutuli24@gmail.com>
2018-02-13 11:08:26 +0300

    changes made to html and script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-var add = function(number1, number2) {
-return number1 + number2;
+var add = function(number1, number2, number3) {
+return number1 + number2 + number3;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var number3 = parseInt(prompt("Enter a third number:"));
 
-alert(add(number1, number2));
+alert(add(number1, number2, number3));
